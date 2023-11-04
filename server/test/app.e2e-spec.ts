bccbc3505a0dd9b38fd31f8ebc66ab31f3aacb9e
Alexander Tupavov <atupavov@gridgain.com>
2023-11-04 10:46:36 +0400

    change module name

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { FeatureModule } from '../src/modules/feature/featureModule';
+import { FeatureModule } from '../src/modules/feature/feature.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
