09998fcdc3627d5fac2e1638c43b5a9851420ba4
Alexander Tupavov <tauinbox@gmail.com>
2025-02-03 18:43:11 +0400

    upgrade client and server to the latest frameworks versions and its dependencies

--- client/src/app/feature/feature.component.ts
+++ client/src/app/feature/feature.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FeatureApiService } from './feature-api.service';
 
 @Component({
-  selector: 'app-feature',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './feature.component.html',
-  styleUrl: './feature.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-feature',
+    imports: [CommonModule],
+    templateUrl: './feature.component.html',
+    styleUrl: './feature.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeatureComponent {
   private readonly featureApi = inject(FeatureApiService);
