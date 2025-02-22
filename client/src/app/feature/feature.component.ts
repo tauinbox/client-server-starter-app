8289a5b10ed688c293353c037b85e46d14a97cc9
Alexander Tupavov <tauinbox@gmail.com>
2025-02-22 12:21:05 +0400

    refactor description and config endpoints

--- client/src/app/feature/feature.component.ts
+++ client/src/app/feature/feature.component.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { FeatureApiService } from './feature-api.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
 
 @Component({
     selector: 'app-feature',
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
     templateUrl: './feature.component.html',
     styleUrl: './feature.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -12,8 +15,13 @@ import { rxResource } from '@angular/core/rxjs-interop';
 export class FeatureComponent {
   private readonly featureApi = inject(FeatureApiService);
 
-  private readonly featureResource = rxResource({loader:  () => this.featureApi.getFeatureEntities()});
+  private readonly descriptionResource = rxResource({loader:  () => this.featureApi.getFeatureDescription()});
+  private readonly configResource = rxResource({loader:  () => this.featureApi.getConfig()});
+  private readonly entitiesResource = rxResource({loader:  () => this.featureApi.getFeatureEntities()});
 
-  protected entities = this.featureResource.value;
-  protected isLoading = this.featureResource.isLoading;
+  protected description = this.descriptionResource.value;
+  protected config = this.configResource.value;
+  protected entities = this.entitiesResource.value;
+
+  protected isLoading = computed(() => this.descriptionResource.isLoading() || this.configResource.isLoading() || this.entitiesResource.isLoading());
 }
