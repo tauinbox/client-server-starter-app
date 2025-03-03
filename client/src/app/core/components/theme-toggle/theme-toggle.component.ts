26e7d6e99dba451a92464c7fa3ce7d21c70e129a
Alexander Tupavov <atupavov@gridgain.com>
2025-03-03 20:31:21 +0400

    UI: implement dark theme support

---
+++ client/src/app/core/components/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatIcon } from '@angular/material/icon';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [
+    MatIconButton,
+    MatTooltip,
+    MatIcon
+  ],
+  templateUrl: './theme-toggle.component.html',
+  styleUrl: './theme-toggle.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ThemeToggleComponent {
+  themeService = inject(ThemeService);
+}
