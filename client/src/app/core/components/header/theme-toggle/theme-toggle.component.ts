6a7b7e7a2819c6f58bab99cce11a4574722a9dc9
Alexander Tupavov <atupavov@gridgain.com>
2025-03-04 21:37:46 +0400

    UI: rearrange styles

--- client/src/app/core/components/header/theme-toggle/theme-toggle.component.ts
+++ client/src/app/core/components/header/theme-toggle/theme-toggle.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatIcon } from '@angular/material/icon';
-import { ThemeService } from '../../services/theme.service';
+import { ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'app-theme-toggle',
