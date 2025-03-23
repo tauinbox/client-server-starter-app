3697f787289db19697fc6a307f39e2cfd377e44f
Alexander Tupavov <atupavov@gridgain.com>
2025-03-23 22:42:44 +0400

    UI: refactor refresh token functionality

--- client/src/app/features/auth/interceptors/jwt.interceptor.ts
+++ client/src/app/features/auth/interceptors/jwt.interceptor.ts
@@ -7,14 +7,14 @@ import {
 } from '@angular/common/http';
 import { catchError, switchMap, throwError } from 'rxjs';
 import { TokenService } from '../services/token.service';
-import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (
   request: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
   const tokenService = inject(TokenService);
-  const authService = inject(AuthService);
+  const router = inject(Router);
   const token = tokenService.getAccessToken();
 
   if (token) {
@@ -23,33 +23,28 @@ export const jwtInterceptor: HttpInterceptorFn = (
 
   return next(request).pipe(
     catchError((error: HttpErrorResponse) => {
-      // Check if the error is 401 Unauthorized and we're not trying to refresh already
       if (
         error.status === 401 &&
         !request.url.includes('refresh-token') &&
         !request.url.includes('login') &&
         !tokenService.isRefreshInProgress()
       ) {
-        tokenService.startRefreshProcess();
-
-        return authService.refreshToken().pipe(
+        return tokenService.refreshToken().pipe(
           switchMap((tokens) => {
             if (!tokens) {
-              authService.logout();
+              void router.navigate(['/login']);
               return throwError(() => error);
             }
 
             return next(addTokenToRequest(request, tokens.access_token));
           }),
           catchError(() => {
-            tokenService.endRefreshProcess(null);
-            authService.logout();
+            void router.navigate(['/login']);
             return throwError(() => error);
           })
         );
       }
 
-      // For all other errors, just pass them through
       return throwError(() => error);
     })
   );
--- client/src/app/features/auth/services/token.service.ts
+++ client/src/app/features/auth/services/token.service.ts
@@ -1,22 +1,42 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  BehaviorSubject,
+  catchError,
+  finalize,
+  map,
+  Observable,
+  of,
+  tap
+} from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
-import { BehaviorSubject } from 'rxjs';
-import { CustomJwtPayload, TokensResponse } from '../models/auth.types';
+import { Router } from '@angular/router';
+import {
+  AuthResponse,
+  CustomJwtPayload,
+  RefreshTokensRequest,
+  TokensResponse
+} from '../models/auth.types';
+import { AUTH_API_V1 } from '@features/auth/services/auth.service';
+import { User } from '@features/users/models/user.types';
 
-const ACCESS_TOKEN_KEY = 'auth_access_token';
-const REFRESH_TOKEN_KEY = 'auth_refresh_token';
+const AUTH_TOKENS = 'auth_tokens';
+
+export const USER_KEY = 'auth_user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
-  private refreshTokenSubject = new BehaviorSubject<TokensResponse | null>(
-    null
-  );
+  private http = inject(HttpClient);
+  private router = inject(Router);
 
   private refreshInProgress = false;
+  private authTokensSubject = new BehaviorSubject<AuthResponse | null>(
+    this.getAuthTokens()
+  );
 
-  refreshToken$ = this.refreshTokenSubject.asObservable();
+  refreshToken$ = this.authTokensSubject.asObservable();
 
   private isAuthenticatedSignal = signal<boolean>(false);
   readonly isAuthenticated = this.isAuthenticatedSignal.asReadonly();
@@ -25,27 +45,56 @@ export class TokenService {
     this.isAuthenticatedSignal.set(!this.isTokenExpired());
   }
 
+  getAuthTokens() {
+    let authTokens: AuthResponse | null = null;
+    const tokens = localStorage.getItem(AUTH_TOKENS);
+
+    try {
+      authTokens = tokens ? (JSON.parse(tokens) as AuthResponse) : null;
+    } catch (error) {
+      console.error('Unable to parse auth token', error);
+    }
+
+    return authTokens;
+  }
+
   getAccessToken(): string | null {
-    return localStorage.getItem(ACCESS_TOKEN_KEY);
+    const response = this.getAuthTokens();
+    return response ? response.tokens.access_token : null;
   }
 
   getRefreshToken(): string | null {
-    return localStorage.getItem(REFRESH_TOKEN_KEY);
+    const response = this.getAuthTokens();
+    return response ? response.tokens.refresh_token : null;
   }
 
-  saveTokens(tokens: TokensResponse): void {
-    localStorage.setItem(ACCESS_TOKEN_KEY, tokens.access_token);
-    localStorage.setItem(REFRESH_TOKEN_KEY, tokens.refresh_token);
-    this.refreshTokenSubject.next(tokens);
+  getUserData(): User | null {
+    const response = this.getAuthTokens();
+    return response ? response.user : null;
+  }
+
+  saveTokens(response: AuthResponse): void {
+    localStorage.setItem(AUTH_TOKENS, JSON.stringify(response));
     this.isAuthenticatedSignal.set(true);
   }
 
   clearTokens(): void {
-    localStorage.removeItem(ACCESS_TOKEN_KEY);
-    localStorage.removeItem(REFRESH_TOKEN_KEY);
-    this.refreshTokenSubject.next(null);
+    localStorage.removeItem(AUTH_TOKENS);
+    this.authTokensSubject.next(null);
     this.isAuthenticatedSignal.set(false);
     this.refreshInProgress = false;
+  }
+
+  logout(): void {
+    if (this.isAuthenticated()) {
+      this.http
+        .post(`${AUTH_API_V1}/logout`, {})
+        .pipe(catchError(() => of(null)))
+        .subscribe();
+    }
+
+    this.clearTokens();
+    void this.router.navigate(['/login']);
   }
 
   isTokenExpired(): boolean {
@@ -66,7 +115,7 @@ export class TokenService {
 
     try {
       const decoded = jwtDecode<CustomJwtPayload>(token);
-      return decoded.exp ? decoded.exp * 1000 : null; // Convert to milliseconds
+      return decoded.exp ? decoded.exp * 1000 : null;
     } catch (error) {
       return null;
     }
@@ -83,17 +132,45 @@ export class TokenService {
     }
   }
 
-  startRefreshProcess() {
-    this.refreshInProgress = true;
-    this.refreshTokenSubject.next({} as TokensResponse);
-  }
-
-  endRefreshProcess(tokens: TokensResponse | null) {
-    this.refreshInProgress = false;
-    this.refreshTokenSubject.next(tokens);
-  }
-
   isRefreshInProgress(): boolean {
     return this.refreshInProgress;
+  }
+
+  refreshToken(): Observable<TokensResponse | null> {
+    if (this.refreshInProgress) {
+      return this.refreshToken$.pipe(
+        map((response) => response?.tokens || null)
+      );
+    }
+
+    const refreshToken = this.getRefreshToken();
+    if (!refreshToken) {
+      this.clearTokens();
+      return of(null);
+    }
+
+    const refreshRequest: RefreshTokensRequest = {
+      refresh_token: refreshToken
+    };
+
+    this.refreshInProgress = true;
+
+    return this.http
+      .post<AuthResponse>(`${AUTH_API_V1}/refresh-token`, refreshRequest)
+      .pipe(
+        tap((response) => {
+          this.saveTokens(response);
+          this.authTokensSubject.next(response);
+        }),
+        map((response) => response.tokens),
+        finalize(() => {
+          this.refreshInProgress = false;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          this.clearTokens();
+          this.authTokensSubject.next(null);
+          return of(null);
+        })
+      );
   }
 }
