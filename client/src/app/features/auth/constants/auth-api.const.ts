7877a609f9c4b0b033637191ca82377c760f2bf1
Alexander Tupavov <atupavov@gridgain.com>
2026-02-08 16:20:19 +0400

    linter fixes

--- client/src/app/features/auth/constants/auth-api.const.ts
+++ client/src/app/features/auth/constants/auth-api.const.ts
@@ -2,8 +2,8 @@ const AUTH_API_V1 = 'api/v1/auth';
 
 export enum AuthApiEnum {
   Login = `${AUTH_API_V1}/login`,
-  Register = `${AUTH_API_V1}/register`,
   Logout = `${AUTH_API_V1}/logout`,
+  Register = `${AUTH_API_V1}/register`,
   Profile = `${AUTH_API_V1}/profile`,
   RefreshToken = `${AUTH_API_V1}/refresh-token`
 }
